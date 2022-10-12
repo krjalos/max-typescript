0ecf18352547781ea2b44b16b6807c3113900821
Alex Antonov <aantonov@cendyn.com>
2022-10-12 17:37:06 +0300

    unknown

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-var add = function (n1, n2) {
-    return n1 + n2;
-};
-var printResults = function (number) {
-    console.log("Result is: " + number);
-};
-printResults(add(3, 5));
-var combinedValues;
-combinedValues = add;
-console.log(combinedValues(7, 1));
-var addAndHandle = function (a, b, callback) {
-    callback(add(a, b));
-};
-addAndHandle(14, 15, printResults);
+var userInput;
+var userName;
+userInput = "Alex";
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
