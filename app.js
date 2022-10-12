5859a74a7881052fcc297f45baeaac5263e9679b
Alex Antonov <aantonov@cendyn.com>
2022-10-12 16:13:20 +0300

    literal types

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var combine = function (input1, input2) {
+var combine = function (input1, input2, returnedType) {
+    if (returnedType === void 0) { returnedType = 'as-number'; }
     var result;
-    if (typeof input1 === 'number' && typeof input2 === "number") {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === "number" || returnedType === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 };
-console.log(combine(5, 10));
+console.log(combine(5, 10, 'as-string'));
 console.log(combine(5, '10'));
-console.log(combine("Alex", 'Beer'));
+console.log(combine("Alex", 'Beer', 'as-string'));
--- app.ts
+++ app.ts
@@ -1,15 +1,17 @@
-const combine = (input1 : number | string, input2: number | string) => {
+const combine = (input1: number | string, input2: number | string, returnedType: 'as-string' | 'as-number' = 'as-number') => {
   let result;
 
-  if(typeof input1 === 'number' && typeof input2 === "number") {
-    result = input1 + input2;
-  }else {
+  if (typeof input1 === 'number' && typeof input2 === "number" || returnedType === 'as-number') {
+    result = +input1 + +input2;
+  } else {
     result = input1.toString() + input2.toString();
   }
 
   return result;
+
+
 }
 
-console.log(combine(5, 10));
+console.log(combine(5, 10, 'as-string'));
 console.log(combine(5, '10'));
-console.log(combine("Alex", 'Beer'));
+console.log(combine("Alex", 'Beer', 'as-string'));
