e46a1b974e4c19da9db04186a2133a1f7ef9ae0f
Alex Antonov <aantonov@cendyn.com>
2022-10-12 10:35:27 +0300

    object type, inference - better to use

---
+++ app.js
@@ -0,0 +1,9 @@
+// const person : {
+//   name : string;
+//   age: number;
+// } = {
+var person = {
+    name: "Alex",
+    age: 38
+};
+console.log(person.name);
---
+++ app.ts
@@ -0,0 +1,10 @@
+// const person : {
+//   name : string;
+//   age: number;
+// } = {
+const person = {
+  name: "Alex",
+  age: 38
+}
+
+console.log(person.name);
