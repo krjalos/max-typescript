04cc19f5981cdd68b5ea9b80ce2d2784009dbc82
Alex Antonov <aantonov@cendyn.com>
2022-10-12 10:52:26 +0300

    Tuples

--- app.js
+++ app.js
@@ -5,8 +5,12 @@
 var person = {
     name: "Alex",
     age: 38,
-    hobbies: ["chess", "swimming"]
+    hobbies: ["chess", "swimming"],
+    role: [1, "author"]
 };
+// person.role.push(10); -- allowed but incorrect
+// person.role[1] = 10;  -- error
+//person.role = [10, 10] -- error
 var favoriteActivities;
 favoriteActivities = ["sports"];
 console.log(person.name);
--- app.ts
+++ app.ts
@@ -2,11 +2,21 @@
 //   name : string;
 //   age: number;
 // } = {
-const person = {
+const person : {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]
+} = {
   name: "Alex",
   age: 38,
-  hobbies: ["chess", "swimming"]
+  hobbies: ["chess", "swimming"],
+  role: [1, "author"]
 }
+
+// person.role.push(10); -- allowed but incorrect
+// person.role[1] = 10;  -- error
+//person.role = [10, 10] -- error
 
 let favoriteActivities: string[];
 
