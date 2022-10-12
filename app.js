2c07528d3a7a8047356d63b1a83b3267c2fc9697
Alex Antonov <aantonov@cendyn.com>
2022-10-12 17:16:25 +0300

    function types

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ var printResults = function (number) {
     console.log("Result is: " + number);
 };
 printResults(add(3, 5));
+var combinedValues;
+combinedValues = add;
+console.log(combinedValues(7, 1));
--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
-const add = (n1: number, n2 : number) => { // inferred function type number
+const add = (n1: number, n2: number) => { // inferred function type number
   return n1 + n2;
 }
 
-const printResults = (number : number) : void => { // type of function that returns nothing
+const printResults = (number: number): void => { // type of function that returns nothing
   console.log("Result is: " + number);
 }
 
 printResults(add(3, 5));
+
+
+let combinedValues : (a: number, b: number) => number;
+
+combinedValues = add;
+
+console.log(combinedValues(7,1));
