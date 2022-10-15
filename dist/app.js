6cff3025715d51c65f25717f31c3500ffe08660e
Alex Antonov <aantonov@cendyn.com>
2022-10-15 21:03:11 +0300

    get & set

--- dist/app.js
+++ dist/app.js
@@ -4,7 +4,13 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _Department_latestPrivate;
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _Department_latestPrivate, _Admins_masterPass;
 class Department {
     constructor(name, id) {
         this.name = name;
@@ -12,6 +18,19 @@ class Department {
         this.employees = [];
         _Department_latestPrivate.set(this, "Test private sting");
         this.name = name;
+    }
+    get latestPrivate() {
+        if (__classPrivateFieldGet(this, _Department_latestPrivate, "f").length > 0) {
+            return __classPrivateFieldGet(this, _Department_latestPrivate, "f");
+        }
+        else {
+            return "Not set";
+        }
+    }
+    set latestPrivate(newString) {
+        if (newString.length > 0) {
+            __classPrivateFieldSet(this, _Department_latestPrivate, newString, "f");
+        }
     }
     printName() {
         console.log(`This department name is ${this.name}`);
@@ -31,11 +50,14 @@ it.addEmployee("Maria");
 it.addEmployee("Mira");
 it.printName();
 it.printLatestPrivate();
+it.latestPrivate = "Latest private is set";
+console.log(it.latestPrivate);
 console.log(it);
 class Admins extends Department {
     constructor(name, id, admins) {
         super(name, id);
         this.admins = admins;
+        _Admins_masterPass.set(this, 123123);
     }
     printAdmins() {
         console.log(this.admins);
@@ -43,9 +65,14 @@ class Admins extends Department {
     addEmployee(name) {
         this.employees.push(name);
     }
+    getMasterPass() {
+        console.log(__classPrivateFieldGet(this, _Admins_masterPass, "f"));
+    }
 }
+_Admins_masterPass = new WeakMap();
 const admins = new Admins("Admins", 2, ["Alex", "Mira"]);
 admins.printAdmins();
 admins.printLatestPrivate();
+admins.getMasterPass();
 console.log(admins);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,20 @@ class Department {
   protected employees : string[] = [];
   #latestPrivate: string = "Test private sting";
 
+  get latestPrivate() {
+    if(this.#latestPrivate.length > 0){
+      return this.#latestPrivate;
+    }else {
+      return "Not set";
+    }
+  }
+
+  set latestPrivate(newString:string) {
+    if(newString.length > 0){
+      this.#latestPrivate = newString;
+    }
+  }
+
   constructor(public name : string, public readonly id: number) {
     this.name = name;
   }
@@ -28,10 +42,15 @@ it.addEmployee("Mira");
 
 it.printName();
 it.printLatestPrivate();
+it.latestPrivate = "Latest private is set";
+
+console.log(it.latestPrivate);
 
 console.log(it);
 
 class Admins extends Department {
+  #masterPass : number = 123123;
+
   constructor(name: string, id: number, public admins: string[]) {
     super(name, id);
   }
@@ -43,11 +62,16 @@ class Admins extends Department {
   addEmployee(name: string) {
     this.employees.push(name);
   }
+
+  getMasterPass() {
+    console.log(this.#masterPass);
+  }
 }
 
 const admins = new Admins("Admins", 2, ["Alex", "Mira"]);
 
 admins.printAdmins();
 admins.printLatestPrivate();
+admins.getMasterPass();
 
 console.log(admins);
