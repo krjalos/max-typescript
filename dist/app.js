fd894d68d881a76272e5ccb2a52f8fac40e61ad6
Alex Antonov <aantonov@cendyn.com>
2022-10-16 21:46:58 +0300

    Advanced types

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,2 @@
 "use strict";
-let add;
-add = (a, b) => {
-    return a + b;
-};
-console.log(add(5, 7));
-class employee {
-    constructor(name, age) {
-        this.name = name;
-        this.position = "Accountant";
-        if (age) {
-            this.age = age;
-        }
-        else { }
-        this.age = 0;
-    }
-    print(connector) {
-        console.log(`${this.name} ${connector} ${this.age}`);
-    }
-}
-let p1;
-p1 = new employee("Alex", 38);
-p1.print("is");
 //# sourceMappingURL=app.js.map
