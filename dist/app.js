e1a526ab683ca68cf18fc089282e8b5425ee4d50
Alex Antonov <aantonov@cendyn.com>
2022-10-17 16:29:42 +0300

    template decorator

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,24 @@
 "use strict";
+// function Logger(identifier: string){
+//   return (constructor: Function) => {
+//     console.log(identifier);
+//     console.log(constructor);
+//   }
+// }
+//
+// @Logger("Logging person")
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(identifier) {
+function Template(customHtml, id) {
     return (constructor) => {
-        console.log(identifier);
-        console.log(constructor);
+        const element = document.getElementById(id);
+        element.innerHTML = customHtml;
+        const person = new constructor();
+        element.querySelector('h1').innerHTML = person.name;
     };
 }
 let Person = class Person {
@@ -18,7 +28,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger("Logging person")
+    Template("<h1></h1>", "root")
 ], Person);
 const alex = new Person();
 console.log(alex);
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
-function Logger(identifier: string){
-  return (constructor: Function) => {
-    console.log(identifier);
-    console.log(constructor);
+// function Logger(identifier: string){
+//   return (constructor: Function) => {
+//     console.log(identifier);
+//     console.log(constructor);
+//   }
+// }
+//
+// @Logger("Logging person")
+
+function Template(customHtml: string, id: string){
+  return (constructor: any) => {
+    const element = document.getElementById(id) as HTMLElement;
+    element!.innerHTML = customHtml;
+    const person = new constructor();
+
+    element!.querySelector('h1')!.innerHTML = person.name;
   }
 }
 
-@Logger("Logging person")
+@Template("<h1></h1>", "root")
 class Person {
   name:string = "Alex";
 
