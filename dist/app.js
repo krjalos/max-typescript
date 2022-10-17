68b92707257281ced29f1d545ee336052cc360f9
Alex Antonov <aantonov@cendyn.com>
2022-10-17 23:02:14 +0300

    Property, descriptor, method, param decorators

--- dist/app.js
+++ dist/app.js
@@ -13,6 +13,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Template(customHtml, id) {
     return (constructor) => {
         const element = document.getElementById(id);
@@ -32,4 +35,50 @@ Person = __decorate([
 ], Person);
 const alex = new Person();
 console.log(alex);
+function Log(target, propertyName) {
+    console.log("_________");
+    console.log(target);
+    console.log(propertyName);
+}
+function Log2(target, name, descriptor) {
+    console.log("_________");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, position) {
+    console.log("_________1");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Course {
+    constructor(title, description) {
+        this._attendees_number = 0;
+        this.title = title;
+        this.description = description;
+        this.id = Math.floor(Math.random() * 100);
+    }
+    set attendees(attendee) {
+        if (attendee > 0) {
+            this._attendees_number = attendee;
+        }
+    }
+    get attendees() {
+        return this._attendees_number;
+    }
+    getIncome(price) {
+        return this._attendees_number * price;
+    }
+}
+__decorate([
+    Log
+], Course.prototype, "title", void 0);
+__decorate([
+    Log2
+], Course.prototype, "attendees", null);
+__decorate([
+    Log2,
+    __param(0, Log3)
+], Course.prototype, "getIncome", null);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -28,3 +28,56 @@ class Person {
 
 const alex = new Person();
 console.log(alex);
+
+
+
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("_________");
+  console.log(target);
+  console.log(propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("_________");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string, position: number) {
+  console.log("_________1");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Course {
+  @Log
+  title: string;
+  id: number;
+  description: string;
+  private _attendees_number: number = 0;
+
+  @Log2
+  set attendees (attendee: number){
+    if(attendee > 0){
+      this._attendees_number = attendee;
+    }
+  }
+
+  get attendees(){
+    return this._attendees_number;
+  }
+
+  constructor(title: string, description: string) {
+    this.title = title;
+    this.description = description;
+    this.id = Math.floor(Math.random() * 100);
+  }
+
+  @Log2
+  getIncome(@Log3 price: number) {
+    return this._attendees_number * price;
+  }
+}
