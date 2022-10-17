d1945ba422c9471b7f4f8eaf1e828d6c533ae0f3
Alex Antonov <aantonov@cendyn.com>
2022-10-17 11:17:08 +0300

    Generic types - basic

--- dist/app.js
+++ dist/app.js
@@ -1,82 +1,13 @@
 "use strict";
-// type Car = {
-//   maxSpeed: number;
-//   tires: string;
-// }
-//
-// type Plane = {
-//   fuel: string;
-//   maxSpeed: number;
-// }
-//
-// type vechicles = Car & Plane;
-//
-// let vechicle: vechicles;
-//
-// vechicle = {
-//   maxSpeed: 100,
-//   tires: "Michelin",
-//   fuel: "diesel"
-// }
-//
-// type FuelType = number | string | boolean;
-// type Tires =  number | boolean;
-//
-// type VechicleParam = FuelType & Tires;
-//
-// let param: VechicleParam;
-//
-// param = 12;
-var _a;
-class Car {
-    drive() {
-        console.log("Driving");
-    }
-}
-class Truck {
-    drive() {
-        console.log("Driving");
-    }
-    load() {
-        console.log("Loading");
-    }
-}
-const v1 = new Car;
-const v2 = new Truck();
-const newVechicle = (vechicle) => {
-    vechicle.drive();
-    if (vechicle instanceof Truck) {
-        vechicle.load();
-    }
-};
-newVechicle(v1);
-newVechicle(v2);
-const userInput = document.getElementById("user-input");
-const userInput2 = document.getElementById("user-input");
-console.log(userInput.value);
-console.log(userInput2.value);
-const v1Owner = {
-    age: 28,
-    licenseNumber: 12345
-};
-function add(a, b) {
-    if (typeof a === "string" || typeof b === "string") {
-        return a.toString() + b.toString();
-    }
-    else {
-        return +a + +b;
-    }
-}
-console.log(add("Alex", " mira").split(" "));
-const fetchUserData = {
-    name: "alex",
-    age: 38,
-    job: {
-        title: "CEO",
-        company: "FIE"
-    }
-};
-console.log((_a = fetchUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-const unknown = "";
-const anotherVar = unknown !== null && unknown !== void 0 ? unknown : "DEFAULT"; // DEFAULT on case unknown is null or undefined
+const exampleGenericArray = [];
+exampleGenericArray[0] = "Test";
+// exampleGenericArray[1] = 2; - not allowed
+const testPromise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Test");
+    }, 2000);
+});
+testPromise.then((result) => {
+    console.log(result);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,104 +1,15 @@
-// type Car = {
-//   maxSpeed: number;
-//   tires: string;
-// }
-//
-// type Plane = {
-//   fuel: string;
-//   maxSpeed: number;
-// }
-//
-// type vechicles = Car & Plane;
-//
-// let vechicle: vechicles;
-//
-// vechicle = {
-//   maxSpeed: 100,
-//   tires: "Michelin",
-//   fuel: "diesel"
-// }
-//
-// type FuelType = number | string | boolean;
-// type Tires =  number | boolean;
-//
-// type VechicleParam = FuelType & Tires;
-//
-// let param: VechicleParam;
-//
-// param = 12;
+const exampleGenericArray: Array<string> = [];
 
-class Car {
-  drive() {
-    console.log("Driving");
-  }
-}
-
-class Truck {
-  drive() {
-    console.log("Driving");
-  }
-
-  load() {
-    console.log("Loading")
-  }
-}
-
-type Vechicle = Car | Truck;
-
-const v1 = new Car;
-const v2 = new Truck();
-
-const newVechicle = (vechicle : Vechicle) => {
-  vechicle.drive();
-  if(vechicle instanceof Truck) {
-    vechicle.load();
-  }
-}
-
-newVechicle(v1);
-newVechicle(v2);
-
-const userInput = <HTMLInputElement>document.getElementById("user-input")!;
-const userInput2 = document.getElementById("user-input")! as HTMLInputElement;
-
-console.log(userInput.value);
-console.log(userInput2.value);
-
-interface Owner {
-  [prop: string]: number;
-}
-
-const v1Owner: Owner = {
-  age: 28,
-  licenseNumber: 12345
-}
-
-type Combinable = string | number;
-
-function add(a: string, b: string) : string;
-function add(a: number, b: number) : number;
-function add(a: Combinable, b: Combinable){
-  if(typeof a === "string" || typeof b === "string"){
-    return a.toString() + b.toString();
-  }else {
-    return +a + +b;
-  }
-}
-
-console.log(add("Alex", " mira").split(" "));
+exampleGenericArray[0] = "Test";
+// exampleGenericArray[1] = 2; - not allowed
 
 
-const fetchUserData = {
-  name: "alex",
-  age: 38,
-  job: { // Optional
-    title:"CEO",
-    company:"FIE"
-  }
-}
+const testPromise = new Promise<string>((resolve) => {
+  setTimeout(() => {
+    resolve("Test");
+  }, 2000);
+});
 
-console.log(fetchUserData.job?.title);
-
-const unknown = "";
-
-const anotherVar = unknown ?? "DEFAULT"; // DEFAULT on case unknown is null or undefined
+testPromise.then((result) => {
+  console.log(result);
+})
