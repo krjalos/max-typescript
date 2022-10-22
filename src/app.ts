9fbb9bb21102c5c9a81543183aafe8fe8d3f29fa
Alex Antonov <aantonov@cendyn.com>
2022-10-22 18:25:48 +0300

    class transformer

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
-import _ from "lodash";
+import 'reflect-metadata';
+import {plainToClass} from "class-transformer";
 
-console.log(_.shuffle([1,2,3]));
+import Product from './product.module';
+
+const loadedProducts = [
+  {title: "Book", price: 12},
+  {title: "Coffee", price: 4},
+  {title: "Chocolate", price: 4}
+]
+
+const products = plainToClass(Product, loadedProducts);
+
+for(const p in products){
+  console.log(products[p].getInformation());
+}
